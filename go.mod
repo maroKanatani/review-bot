83b4fdd49db4ddaa7b623994cf736fa98d204f47
Kanatani28 <5genbassnohito.maroro0208@gmail.com>
2020-05-03 14:18:57 +0000

    add echo pkg/errors

--- go.mod
+++ go.mod
@@ -2,4 +2,16 @@ module review-bot
 
 go 1.14
 
-require github.com/slack-go/slack v0.6.4
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/slack-go/slack v0.6.4
+	github.com/valyala/fasttemplate v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79 // indirect
+	golang.org/x/net v0.0.0-20200501053045-e0ff5e5a1de5 // indirect
+	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3 // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
-	"net/http"
+	"log"
 	"os"
 
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/pkg/errors"
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
 )
@@ -15,38 +17,67 @@ import (
 var token = os.Getenv("TOKEN")
 var api = slack.New(token)
 
+type URLVeri struct {
+	token     string `json:"token"`
+	challenge string `json:"challenge"`
+	_type     string `json:"type"`
+}
+
+func handle(c echo.Context) error {
+	log.Println("Hello")
+	r := c.Request()
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	body := buf.String()
+	eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: "TOKEN"}))
+	if e != nil {
+		errLog(e)
+		return e
+	}
+
+	if eventsAPIEvent.Type == slackevents.URLVerification {
+		var param URLVeri
+		if err := c.Bind(param); err != nil {
+			errLog(err)
+			return err
+		}
+		return c.String(200, param.challenge)
+	}
+
+	if eventsAPIEvent.Type == slackevents.CallbackEvent {
+		innerEvent := eventsAPIEvent.InnerEvent
+		switch ev := innerEvent.Data.(type) {
+		case *slackevents.AppMentionEvent:
+			api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
+		}
+	}
+
+	return c.String(200, "OK")
+}
+
 func main() {
-	http.HandleFunc("/events-endpoint", func(w http.ResponseWriter, r *http.Request) {
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(r.Body)
-		body := buf.String()
-		eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: "TOKEN"}))
-		if e != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-
-		if eventsAPIEvent.Type == slackevents.URLVerification {
-			var r *slackevents.ChallengeResponse
-			err := json.Unmarshal([]byte(body), &r)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-			w.Header().Set("Content-Type", "text")
-			w.Write([]byte(r.Challenge))
-		}
-		if eventsAPIEvent.Type == slackevents.CallbackEvent {
-			innerEvent := eventsAPIEvent.InnerEvent
-			switch ev := innerEvent.Data.(type) {
-			case *slackevents.AppMentionEvent:
-				api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
-			}
-		}
-	})
-	fmt.Println("[INFO] Server listening")
-
+	// fmt.Println("[INFO] Server listening")
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
-	http.ListenAndServe(":"+port, nil)
+	// http.ListenAndServe(":"+port, nil)
+
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/events-endpoint", handle)
+	e.POST("/events-endpoint", handle)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":" + port))
+}
+
+func errLog(err error) {
+	log.Printf("%+v\n", errors.WithStack(err))
 }
